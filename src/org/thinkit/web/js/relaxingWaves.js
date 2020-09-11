1ac4ce6f73147bb5d555de4899b83a650cca444d
Kato Shinya <yourdream28@gmail.com>
2020-09-11 10:31:04 +0900

    update

--- src/org/thinkit/web/js/relaxingWaves.js
+++ src/org/thinkit/web/js/relaxingWaves.js
@@ -1,7 +1,13 @@
 "use strict";
 
+/**
+ * 波を格納するコンテナ名
+ */
 const CONTAINER_NAME = "relaxing-waves-container";
 
+/**
+ * HTMLにクラス名として "relaxing-waves-container" が指定されたタグ内に波を生成します。
+ */
 const createRelaxingWaves = () => {
   const containerObjects = document.getElementsByClassName(CONTAINER_NAME);
 
