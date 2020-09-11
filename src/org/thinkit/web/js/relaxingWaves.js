6d10d17671774d9558b83ef79a784896cad34a40
Kato Shinya <yourdream28@gmail.com>
2020-09-11 10:28:17 +0900

    add

---
+++ src/org/thinkit/web/js/relaxingWaves.js
@@ -0,0 +1,9 @@
+"use strict";
+
+const CONTAINER_NAME = "relaxing-waves-container";
+
+const createRelaxingWaves = () => {
+  const containerObjects = document.getElementsByClassName(CONTAINER_NAME);
+
+  containerObjects.array.forEach((container) => {});
+};
