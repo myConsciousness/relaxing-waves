62d5cb297a3ddb326703e57b34060285461b5a53
Kato Shinya <yourdream28@gmail.com>
2020-09-11 13:32:46 +0900

    update

--- src/org/thinkit/web/js/relaxingWaves.js
+++ src/org/thinkit/web/js/relaxingWaves.js
@@ -11,6 +11,7 @@
  * or implied. See the License for the specific language governing permissions and limitations under
  * the License.
  */
+
 "use strict";
 
 /**
@@ -19,10 +20,95 @@
 const CONTAINER_NAME = "relaxing-waves-container";
 
 /**
+ * 波の初期値
+ */
+const INITIAL_WAVES = [
+  { x: 48, y: 0, fill: "rgba(255,255,255,0.7" },
+  { x: 48, y: 3, fill: "rgba(255,255,255,0.5)" },
+  { x: 48, y: 5, fill: "rgba(255,255,255,0.3" },
+  { x: 48, y: 7, fill: "#fff" },
+];
+
+/**
  * HTMLにクラス名として "relaxing-waves-container" が指定されたタグ内に波を生成します。
  */
 const createRelaxingWaves = () => {
   const containerObjects = document.getElementsByClassName(CONTAINER_NAME);
 
-  containerObjects.array.forEach((container) => {});
+  Array.from(containerObjects).forEach((container) =>
+    createSvgObject(container)
+  );
 };
+
+const createSvgObject = (container) => {
+  const svgObject = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "svg"
+  );
+
+  svgObject.setAttribute("class", "waves");
+  svgObject.setAttribute("xmlns", "http://www.w3.org/2000/svg");
+  svgObject.setAttribute("xmlns:xlink", "http://www.w3.org/1999/xlink");
+  svgObject.setAttribute("viewBox", "0 24 150 28");
+  svgObject.setAttribute("preserveAspectRatio", "none");
+  svgObject.setAttribute("shape-rendering", "auto");
+
+  svgObject.appendChild(getDefsObject());
+  svgObject.appendChild(getSvgGroupObject());
+
+  container.appendChild(svgObject);
+};
+
+const getDefsObject = () => {
+  const pathObject = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "path"
+  );
+
+  pathObject.setAttributeNS(null, "id", "gentle-wave");
+  pathObject.setAttributeNS(
+    null,
+    "d",
+    "M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z"
+  );
+
+  const defsObject = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "defs"
+  );
+
+  defsObject.appendChild(pathObject);
+
+  return defsObject;
+};
+
+const getSvgGroupObject = () => {
+  const svgGroupObject = document.createElementNS(
+    "http://www.w3.org/2000/svg",
+    "g"
+  );
+
+  svgGroupObject.setAttributeNS(null, "class", "parallax");
+
+  INITIAL_WAVES.forEach((waves) => {
+    const useObject = document.createElementNS(
+      "http://www.w3.org/2000/svg",
+      "use"
+    );
+
+    Object.keys(waves).forEach((attribute) => {
+      useObject.setAttributeNS(
+        "http://www.w3.org/1999/xlink",
+        "xlink:href",
+        "#gentle-wave"
+      );
+      useObject.setAttributeNS(null, attribute, waves[attribute]);
+    });
+
+    svgGroupObject.appendChild(useObject);
+  });
+
+  return svgGroupObject;
+};
+
+createRelaxingWaves();
